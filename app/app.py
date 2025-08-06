aa0b980254811b54f1455fd4490b37bb2654a0a2
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-08-06 15:55:41 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,22 @@
 
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    try:
+        conn = psycopg2.connect(
+            dbname=os.environ.get("POSTGRES_DB"),
+            user=os.environ.get("POSTGRES_USER"),
+            password=os.environ.get("POSTGRES_PASSWORD"),
+            host="db"
+        )
+        return "✅ Connected to PostgreSQL!"
+    except Exception as e:
+        return f"❌ Error: {e}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
